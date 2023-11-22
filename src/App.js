134fe8b00e1fcab610f0045ec22c26b9af268a8b
Yohann <r.yohann91@gmail.com>
2023-11-22 14:09:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState} from 'react';
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const onChangeHandler = function (event) {
+    setInputValue(event.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="bg-slate-900 min-h-screen pt-5 px-10">
+      <h1 className="text-slate-50 text-3xl font-bold text-center">ToDo-App</h1>
+      <form className="flex justify-around items-center gap-4 ">
+        <input
+          type="text"
+          className="w-full md:w-2/3"
+          onChange={onChangeHandler}
+          value={inputValue}
+        />
+        <input
+          type="submit"
+          value="Add Task"
+          className="text-yellow-400 text-xl"
+        />
+      </form>
+      <p className='text-slate-50 text-3xl font-bold'>{inputValue}</p>
+    </main>
   );
 }
 
