dcb7136a7f8992c7f95953786f80d28a574ce5c3
Yohann <r.yohann91@gmail.com>
2023-11-23 13:34:15 +0100

    splice?

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
 import TaskItem from "./components/TaskItem";
 import Form from "./components/Form";
 import Logo from "./components/Logo";
-import { useState } from "react";
-
-
+import { useEffect, useState } from "react";
 
 function App() {
-    const [textEntered, setTextEntered] = useState('');
-    const [tasks,setTasks] = useState([])    
-    const onChangeHandler = function(event){
-        setTextEntered(event.target.value)
-    }
-    const addTaskHandler = function(event){
-        event.preventDefault();
-        setTasks([...tasks, textEntered])
-    }
- 
+  const [textEntered, setTextEntered] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const taskList = JSON.parse(localStorage.getItem("my-tasks"));
+
+    setTasks(taskList);
+  }, []);
+
+  const onChangeHandler = function (event) {
+    setTextEntered(event.target.value);
+  };
+  const addTaskHandler = function (event) {
+    event.preventDefault();
+    const newArr = [...tasks, textEntered];
+    setTasks(newArr);
+
+    localStorage.setItem("my-tasks", JSON.stringify(newArr));
+  };
+  const removeItem = function(index){
+// splice
+  }
+
   return (
     <main className="bg-slate-900 min-h-screen pt-5 px-10">
-      <Logo img={'/img/logo.png'}/>
+      <Logo img={"/img/logo.png"} />
 
-      <Form onChangeHandler={onChangeHandler} addTaskHandler={addTaskHandler}/>
+      <Form onChangeHandler={onChangeHandler} addTaskHandler={addTaskHandler} />
 
       <section className="mt-10 md:w-2/3 mx-auto ">
         <ul className="flex flex-col space-y-5">
           {tasks.map((item, index) => (
-            <TaskItem key={index} name={item}></TaskItem>
+            <TaskItem key={index} removeItem={() => removeItem(index)} name={item}></TaskItem>
           ))}
         </ul>
       </section>
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -3,6 +3,7 @@ export default function TaskItem(props) {
       <li
         className="bg-slate-100 text-center py-4 rounded shadow shadow-slate-400">
         {props.name}
+        <img onClick={props.removeItem} src="/img/remove.png" className=" w-5 inline pl-1 cursor-pointer"></img>
       </li>
     );
   }
