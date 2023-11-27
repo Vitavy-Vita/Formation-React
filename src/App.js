a82da1875d52841b14491841db604d006e3ec715
Vitavy <127411106+Vitavy-Vita@users.noreply.github.com>
2023-11-27 22:31:11 +0100

    everything working, looks like shit tho

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Inputs from "./components/Inputs";
 import Liste from "./components/Liste";
-import RangeSlider from 'react-range-slider-input';
-import 'react-range-slider-input/dist/style.css';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Slider from "@mui/material/Slider";
 import { FaRegCopy } from "react-icons/fa6";
 import { useState } from "react";
 function App() {
   const [password, setPassword] = useState("");
-  const [length, setLength] = useState(0,12);
+  const [length, setLength] = useState(0, 20);
+
   const [includeLowercase, setIncludeLowercase] = useState(false);
   const [includeUppercase, setIncludeUppercase] = useState(false);
   const [includeNumbers, setIncludeNumbers] = useState(false);
@@ -35,22 +37,41 @@ function App() {
     if (includeNumbers) validChars += numberChars;
     if (includeSymbols) validChars += specialChars;
 
-    const newPassword = Array.from({ length }, () => {
+    const newPassword = Array.from({ length: length }, () => {
       const randomIndex = Math.floor(Math.random() * validChars.length);
       return validChars.charAt(randomIndex);
     }).join("");
 
     setPassword(newPassword);
   };
-
+  function valuetext(value) {
+    return `${value}°C`;
+  }
   return (
     <main className="bg-blue min-h-screen mx:auto">
       <h1 className="text-white text-center ">PassSafe Maker</h1>
       <section className="bg-white w-2/4 ">
         <Inputs value={password} />
-        <FaRegCopy className="" onClick={() => {navigator.clipboard.writeText(password)}}/>
-        <h2>Character Length</h2>
-        <RangeSlider className='' value={length}/>
+        <FaRegCopy
+          className=""
+          onClick={() => {
+            navigator.clipboard.writeText(password);
+          }}
+        />
+        <h2>Character Length: {length}</h2>
+        <Box sx={{ width: 300 }}>
+          <Slider
+            aria-label="Temperature"
+            defaultValue={30}
+            getAriaValueText={valuetext}
+            max={20}
+            track={false}
+            value={length}
+            onChange={(e, newValue) => {
+              setLength(newValue);
+            }}
+          />
+        </Box>
 
         <Liste
           options={"include Uppercase Letters"}
