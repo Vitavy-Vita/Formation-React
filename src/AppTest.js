a15f37126cbc637420de4e99466773ee48445aaa
Yohann <r.yohann91@gmail.com>
2023-11-28 11:42:29 +0100

    AppTest = reducer

--- src/AppTest.js
+++ src/AppTest.js
@@ -1,33 +1,83 @@
-import TaskItem from "./components/TaskItem";
-import Form from "./components/Form";
-import Title from "./components/Title";
-import { useState } from "react";
+import { useState, useEffect, useRef, useReducer } from 'react';
+import TaskItem from './components/TaskItem';
+
+/* Create reducer qui prend en paramétre de fonction
+`state` = c'est un objet qui représente les états locaux
+`action` = c'est un objet qui contient 
+`type` permettant de définir les différentes action possible
+*/
+const reducer = function (state, action) {
+  switch (action.type) {
+    case 'onChange':
+      return { textEntered: action.payload };
+
+    case 'addTask':
+      return { tasks: [...state.tasks, state.textEntered] };
+
+    default:
+      break;
+  }
+};
 
 function App() {
-    const [textEntered, setTextEntered] = useState('');
-    const [tasks,setTasks] = useState([])    
-    const onChangeHandler = function(event){
-        setTextEntered(event.target.value)
-    }
-    const addTaskHandler = function(event){
-        event.preventDefault();
-        setTasks([...tasks, textEntered])
-    }
- 
+  const initialValue = { tasks: [], textEntered: '' };
+  const [state, dispatch] = useReducer(reducer, initialValue);
+
+  console.log(state);
+
+  const onChangeHandler = (e) =>
+    dispatch({ type: 'onChange', payload: e.target.value });
+
   return (
     <main className="bg-slate-900 min-h-screen pt-5 px-10">
-      <Title name={"ToDo-App"} />
+      {/*       <h1 className="text-slate-50 text-3xl font-bold text-center mb-10">
+        TODO APP
+      </h1> */}
+      {/*  <img src={logo} alt="image" className="block mx-auto" /> */}
+      <img src={'/images/logo.png'} alt="image" className="block mx-auto" />
 
-      <Form onChangeHandler={onChangeHandler} addTaskHandler={addTaskHandler}/>
+      {/* Input pour taper une tache */}
+      <form
+        //onSubmit={addTaskHandler}
+        className="flex justify-center items-center gap-4">
+        <input
+          // A chaque saisi sur le input la fonction est rééxecuter
+          // Avec le paramétre `event` on peut accéder à l'élément `input`
+          // Donc à sa valeur `event.target.value`
+          onChange={onChangeHandler}
+          value={state.textEntered}
+          type="text"
+          className="w-full md:w-2/3"
+          // ref={inputRef}
+        />
+        <input
+          type="submit"
+          value="Add Task"
+          className="text-yellow-400 text-2xl"
+        />
+      </form>
 
       <section className="mt-10 md:w-2/3 mx-auto ">
         <ul className="flex flex-col space-y-5">
-          {tasks.map((item, index) => (
-            <TaskItem key={index} name={item}></TaskItem>
-          ))}
+          {/* 
+          La méthode `map` va parcourrir le liste (tableau) et retourner (afficher) les éléments de la liste (tableau) dans le DOM
+          Elle attend une fonction en `callback` la fonction recevra
+          chaque élément du tableau via les paramétres
+          Donc dans notre exemple `item` représente chaque élément de la liste `tasks`
+          */}
         </ul>
       </section>
     </main>
   );
 }
+
+/* 
+
+Créer un composant nommé `TaskItem` qui représente le UI de chaque `item`
+de la liste `tasks` 
+
+
+
+*/
+
 export default App;
