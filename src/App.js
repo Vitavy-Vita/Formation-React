7fbbadb6bd60b0201e0d178202d10c517f1af614
Yohann <r.yohann91@gmail.com>
2023-11-29 16:16:04 +0100

    new branch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import React from 'react';
-import Card from './components/Card';
-import data from './assets/data';
+import React from "react";
+import Card from "./components/Card";
+import data from "./assets/data";
+import { useReducer } from "react";
+const initialeValue = {
+  textEntered: "",
+  searchChar: data,
+};
+const reducer = function (state, action) {
+  switch (action.type) {
+    case "onChange":
+      const searchArr = data.filter((item) =>
+        item.name.toLowerCase().includes(action.payload.toLowerCase())
+      );
+      return { ...state, textEntered: action.payload, searchChar: searchArr };
+    default:
+      break;
+  }
+};
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialeValue);
+
+  const handleChange = function (e) {
+    dispatch({ type: "onChange", payload: e.target.value });
+  };
   return (
     <main className="p-10">
-      <h1 className="text-2xl text-gray-100 text-center mb-10 uppercase font-mono">Characters ⚡️</h1>
+      <h1 className="text-2xl text-gray-100 text-center mb-10 uppercase font-mono">
+        Characters ⚡️
+      </h1>
+      <form action="" onChange={handleChange}>
+        <input type="text" onChange={handleChange} />
+      </form>
       <div className="grid md:grid-cols-3 gap-5 ">
-        {data.map((item) => (
-          <Card key={item.name} image={item.image} name={item.name} description={item.description} />
+        {state.searchChar.map((item) => (
+          <Card
+            key={item.name}
+            image={item.image}
+            name={item.name}
+            description={item.description}
+          />
         ))}
       </div>
     </main>
