ecb5f87ec06dbeb915884a2a7255f5231fd4064c
Vitavy <127411106+Vitavy-Vita@users.noreply.github.com>
2023-11-30 11:50:46 +0100

    assets import

---
+++ src/pages/BlogPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import data from '../assets/data'
+
+const blogs = [
+  {
+    name:''
+  }
+]
+export default function BlogPage() {
+  return (
+    <div>Blog Page</div>
+    
+  )
+}
