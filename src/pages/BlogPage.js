ddad259fcf894febd46ee5efbb68da054c506b06
Vitavy <127411106+Vitavy-Vita@users.noreply.github.com>
2023-11-30 12:12:23 +0100

    useParams et useNavigate

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import data from '../assets/data'
+import {Link} from 'react-router-dom'
 
 const blogs = [
   {
@@ -8,7 +9,16 @@ const blogs = [
 ]
 export default function BlogPage() {
   return (
-    <div>Blog Page</div>
-    
+    <div className='flex gap-5'>
+    {data.map((item, index) =>(
+      <div key={item.name} className='bg-gray-300 p-5'>
+        <h2>{item.name}</h2>
+        <p>{item.description}</p>
+        <Link to={`/blogs/${index}`} className="bg-white p-2 block mt-8">
+            Detail
+          </Link>
+      </div>
+    ))}
+    </div>
   )
 }
--- src/pages/DetailBlog.js
+++ src/pages/DetailBlog.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import data from "../assets/data";
 
 export default function DetailBlog() {
+  /* 
+  useParams permet d'accéder au paramétre du chemin défini dans le Routes 
+  il retourne un objet (key, value) correspondant au nom du chemin dynamique
+  voir App.js (<Route path=":id" element={<DetailBlog />} />)
+  */
+  const params = useParams();
+  const navigate = useNavigate();
+  const character = data[params.id];
+  const goBack = () => navigate("/blogs");
+
+  if (!character) return <h1>This character doesn't exist</h1>;
   return (
-    <div>Detail Blog</div>
-  )
+    <div>
+      <ul>
+        <li>Name:{character.name}</li>
+        <li>Description:{character.description}</li>
+      </ul>
+      <button className="bg-indigo-500 p-4 my-8" onClick={goBack}>
+        ◀️ GO BACK
+      </button>
+    </div>
+  );
 }
