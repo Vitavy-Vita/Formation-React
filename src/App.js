fd3799328f6fc00a8d43ce9db2d8813e07bda9ca
Vitavy <127411106+Vitavy-Vita@users.noreply.github.com>
2023-11-30 10:32:08 +0100

    started working on router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './index.css';
+import {Route, Routes} from 'react-router-dom'
 
+const Home = function() {
+  return <h1>Home Page</h1>
+}
+const Blog = function() {
+  return <h1>Blog Page</h1>
+}
 function App() {
-  return 
+
+  return (
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/blog' element={<Blog/>}/>
+    </Routes>
+  )
 }
 
 export default App;
