df4aea9327f26d68ce6edfdaddca575d0a6aa07a
Vitavy <127411106+Vitavy-Vita@users.noreply.github.com>
2023-12-01 15:10:28 +0100

    cours useContext

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import './index.css';
+import Accordion from "./components/Accordion";
+import AccordionProvider from "./context/AccordionContext";
+import CartProvider from "./context/CartContext";
+import DisplayCarts from "./components/Card/DisplayCarts";
+//rfc
+export default function App() {
+  /* 
+IMPORTANT
 
-function App() {
+On ne peut pas utiliser le context sur le composant qui l'utilise
+*/
+
   return (
-    <div className='text-indigo-500'>
-      Hello
-    </div>
+    <AccordionProvider>
+      <main className="container mx-auto">
+        <h1>Accordion </h1>
+        <Accordion />
+        {/* ----------------- */}
+
+        <CartProvider>
+          <div>
+            <h2>Cart Shop</h2>
+
+            <DisplayCarts />
+          </div>
+        </CartProvider>
+      </main>
+    </AccordionProvider>
   );
 }
-
-export default App;
